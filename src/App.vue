d2663f943845aa2f800ca2ff571c30e86389f097
NEBULA1978 <rpascualey@gmail.com>
2023-09-05 23:33:31 +0200

    Prueba 1 componente con vue

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+import Listitem from './components/Listitem.vue';
+import {ref} from 'vue'
+
+const arr = ref([
+  {id:0, msg: 'hello world'},
+  {id:1, msg: 'message 2'},
+  {id:2, msg: 'learnvue'},
+  {id:3, msg: 'next mesage'},
+  {id:4, msg: 'yay'},
+])
+
+const activeId = ref(0)
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+  
+  <main>
+  <button @click="activeId = (activeId + 1)% arr.length">Pulsar</button>
+  <ul>
+    <Listitem
+    v-for="msg in arr"
+    :key="msg.id"
+    :id="msg.id"
+    :msg="msg.msg"
+    :active-id="activeId"
+    />
+  </ul>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Listitem.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onUpdated } from 'vue';
+const props=defineProps({
+    id:Number,
+    msg: String,
+    activeId: Number,
+})
+
+onUpdated(() => {
+    // will print wheever our component update
+    console.log('updating' + props.id)
+})
+</script>
+
+<template>
+    <li>
+        <span v-if="id === activeId"></span>
+        {{ msg }}
+    </li>
+</template>
